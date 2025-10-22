a643f80a0a3c4215812f69d5c8dfe67cd8cce3fe
Yash Pawar <yashp6559@gmail.com>
2025-10-22 21:41:04 +0530

    basic Camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Vector3 offset;
+    public PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+       offset = new Vector3(0, 5, -15); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 targetPos = player.transform.position + offset;
+        targetPos.x = transform.position.x; // keep camera x constant
+        transform.position = targetPos;
+
+    
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    [SerializeField] private float speed = 2000f;
+    [SerializeField] private float switchSpeed = 1000f;
+    Rigidbody rb;
+
+    private bool isonleft = false;
+
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
-    {
-        
+    {     
+        rb.AddForce(new Vector3(0, 0, speed * Time.deltaTime));
+
+        if(Input.GetMouseButton(0))
+        {
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                transform.position = Vector3.Slerp(transform.position, new Vector3(-5, transform.position.y, transform.position.z), switchSpeed * Time.deltaTime);
+                isonleft = true;
+            }
+
+            if (Input.mousePosition.x > Screen.width / 2 && isonleft == true)
+            {
+                transform.position = Vector3.Slerp(transform.position, new Vector3(0, transform.position.y, transform.position.z), switchSpeed * Time.deltaTime);
+                isonleft = false;
+            }
+            
+        }
+
     }
+    
 }
